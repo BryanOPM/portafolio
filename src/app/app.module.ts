d6f9baa27f982fbaf7771592aee5a6f4ee7212d7
Bryan Peña <bryan.opm213@gmail.com>
2018-06-01 15:07:06 -0600

    input buscar y productos url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FootersComponent } from './components/footers/footers.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProductoComponent } from './components/producto/producto.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { ProductoComponent } from './components/producto/producto.component';
     FootersComponent,
     PortafolioComponent,
     AboutComponent,
-    ProductoComponent
+    ProductoComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
