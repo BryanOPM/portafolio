a702889c27f4311009a219f9cc83c5a45e533345
Bryan Peña <bryan.opm213@gmail.com>
2018-06-05 16:59:35 -0600

    Primera version

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ProductosService} from "../../services/productos.service";
 
+
+
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
@@ -18,13 +20,13 @@ export class ProductoComponent {
     route.params.subscribe(parametros=>{
 
       // console.log( parametros );
-      console.log( parametros ['id'] );
+      // console.log( parametros ['id'] );
 
       _ps.cargar_producto(parametros ['id'] )
         .subscribe(res=>{
             this.cod = parametros['id'];
             this.producto=res.json();
-            console.log(this.producto );
+            // console.log(this.producto );
       });
     })
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,11 +5,45 @@ import {Http} from "@angular/http";
 export class ProductosService {
 
   productos:any[]=[];
+  productos_filtrado:any[] = [];
   cargando:boolean =true;
 
   constructor( private http:Http ) {
     this.cargar_productos();
 
+   }
+
+   public buscar_producto(termino:string){
+     // console.log("Buscar producto");
+     // console.log(this.productos.length);
+
+     if(this.productos.length===0){
+       this.cargar_productos().then(()=> {
+         //Termino la carga
+         this.filtrar_productos(termino);
+       });
+     }else{
+       this.filtrar_productos(termino);
+     }
+
+
+
+
+   }
+
+   private filtrar_productos(termino:string){
+     this.productos_filtrado = [];
+
+     termino = termino.toLowerCase();
+     this.productos.forEach(prod =>{
+
+       if(prod.categoria.indexOf(termino) >=0 || prod.titulo.toLowerCase().indexOf(termino) >=0){
+         this.productos_filtrado.push(prod);
+         // console.log(prod);
+       }
+
+       // console.log(prod);
+     });
    }
 
    public cargar_producto(cod:string){
@@ -18,7 +52,8 @@ export class ProductosService {
 
 public cargar_productos(){
 
-  this.cargando=true;
+  let promesa=new Promise((resolve, reject)=>{
+
     this.http.get("https://paginaweb-6750f.firebaseio.com/productos_idx.json")
         .subscribe(res=> {
 
@@ -27,11 +62,13 @@ public cargar_productos(){
           // setTimeout(()=>{
             this.cargando=false;
             this.productos=res.json();
+            resolve();
           // }, 1500)
 
-        })
+        });
 
-
+  });
+  return promesa;
 }
 
 }
